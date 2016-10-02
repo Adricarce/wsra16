3b29d239e83f89bdd9cfd3100c754ae82bb7891a
Adri Carcedo <acarcedo003@ikasle.ehu.eus>
2016-10-02 18:37:40 +0200

    labo2

---
+++ Enroll.php
@@ -0,0 +1,22 @@
+<?php
+$link = mysqli_connect ("mysql.hostinger.es","u885903313_adri","Fwrzd7QxoO","u885903313_quizz");
+//$link = mysqli_connect ("localhost","root","","quizz");
+
+if ($link->connect_error) {
+    printf("Connection failed: " . $link->connect_error);
+} 
+
+$sql="INSERT INTO erabiltzaile (Izena, Abizena1, Abizena2, Eposta, Pasahitza, Telefonoa, Espezialitatea, Interesa) VALUES ('$_POST[Izena]' , '$_POST[Abizena1]' , '$_POST[Abizena2]' , '$_POST[Eposta]' , '$_POST[Pasahitza]' , '$_POST[Telefonoa]' , '$_POST[Espezialitatea]' , '$_POST[Interesa]')";
+$ema=mysqli_query($link,$sql);
+
+if (!$ema){
+	echo "<p><a href = 'signUp.html'>Zuzendu datuak</a></p>";
+	die("<p>Errorea gertatu da: ".$link-> error ."</p>");
+}
+else{
+	echo '<div align="center">Erregistro bat gorde  da!
+	<p><a href = "ShowUsers.php">Erabiltzaile guztiak ikusi</a></p>
+	<p><a href = "layout.html">Hasiera orria</a></p></div>';
+}
+mysqli_close($link);
+?>
---
+++ ShowUsers.php
@@ -0,0 +1,33 @@
+<?php
+$link = mysqli_connect ("mysql.hostinger.es","u885903313_adri","Fwrzd7QxoO","u885903313_quizz");
+//$link = mysqli_connect ("localhost","root","","quizz");
+$erabiltzaileak = $link -> query ("SELECT * FROM erabiltzaile");
+echo '<h1 align="center"> Erabiltzaile guztien zerrenda </h1>';
+echo '<table border=1 align="center">
+<tr>
+	<th> Izena </th>
+	<th> Abizena1 </th>
+	<th> Abizena2 </th>
+	<th> Eposta </th>
+	<th> Pasahitza </th>
+	<th> Telefonoa </th>
+	<th> Espezialitatea </th>
+	<th> Interesa </th>
+</tr>';
+
+while( $row = mysqli_fetch_array($erabiltzaileak)) {
+	echo '<tr>
+			<td>'.$row['Izena'].'</td> 
+			<td>'.$row['Abizena1'].'</td> 
+			<td>'.$row['Abizena2'].'</td>
+			<td>'.$row['Eposta'].'</td>
+			<td>'.$row['Pasahitza'].'</td>
+			<td>'.$row['Telefonoa'].'</td>
+			<td>'.$row['Espezialitatea'].'</td>
+			<td>'.$row['Interesa'].'</td>
+		  </tr>';
+}
+echo '</table>';
+echo '<p align="center"><a href = "layout.html">Hasiera orria</a></p></div>';
+$link->close();
+?>
