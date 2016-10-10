e6542543a8f0f99a8d399c999eb1c2ffbbd34524
Adri Carcedo <acarcedo003@ikasle.ehu.eus>
2016-10-10 12:46:23 +0200

    lab3a

--- Enroll.php
+++ Enroll.php
@@ -6,13 +6,50 @@ if ($link->connect_error) {
     printf("Connection failed: " . $link->connect_error);
 } 
 
-if ($_POST['Espezialitatea']!='Besterik'){
+
+if (filter_var($_POST['Izena'],FILTER_VALIDATE_REGEXP,array("options"=>array("regexp"=>"/([A-ZÑÁÉÍÓÚ]{1}[a-zñáéíóú]+(\s){0,1})+/")))){
+	if (filter_var($_POST['Abizena1'],FILTER_VALIDATE_REGEXP,array("options"=>array("regexp"=>"/([A-ZÑÁÉÍÓÚ]{1}[a-zñáéíóú]+(\s){0,1})+/")))){
+		if (filter_var($_POST['Abizena2'],FILTER_VALIDATE_REGEXP,array("options"=>array("regexp"=>"/([A-ZÑÁÉÍÓÚ]{1}[a-zñáéíóú]+(\s){0,1})+/")))){
+			if (filter_var($_POST['Eposta'],FILTER_VALIDATE_REGEXP,array("options"=>array("regexp"=>"/[a-z]+[0-9]{3}@ikasle\.ehu\.e(us|s)/")))){
+				if (filter_var($_POST['Pasahitza'],FILTER_VALIDATE_REGEXP,array("options"=>array("regexp"=>"/[a-zA-Z,0-9]{6,}/")))){
+					if (filter_var($_POST['Telefonoa'],FILTER_VALIDATE_REGEXP,array("options"=>array("regexp"=>"/[0-9]{9}/")))){
+						if ($_POST['Espezialitatea']!='Besterik'){
+							$sql="INSERT INTO erabiltzaile (Izena, Abizena1, Abizena2, Eposta, Pasahitza, Telefonoa, Espezialitatea, Interesa) VALUES ('$_POST[Izena]' , '$_POST[Abizena1]' , '$_POST[Abizena2]' , '$_POST[Eposta]' , '$_POST[Pasahitza]' , '$_POST[Telefonoa]' , '$_POST[Espezialitatea]' , '$_POST[Interesa]')";
+						}
+						else{
+							$sql="INSERT INTO erabiltzaile (Izena, Abizena1, Abizena2, Eposta, Pasahitza, Telefonoa, Espezialitatea, Interesa) VALUES ('$_POST[Izena]' , '$_POST[Abizena1]' , '$_POST[Abizena2]' , '$_POST[Eposta]' , '$_POST[Pasahitza]' , '$_POST[Telefonoa]' , '$_POST[BesteEspezialitatea]' , '$_POST[Interesa]')";
+						}
+					}else{
+						echo 'Telefonoak 9 digitu izan behar ditu.';
+					}
+				}
+				else{
+					echo'Pasahitzak gutxienez 6ko luzera izan behar du.';
+				}
+			}
+			else{
+				echo'Posta elektronikoak EHUko ikasleen txantiloira doitu behar du; hots Hizkiak+3 digitu+ “@ikasle.ehu.” + “eus”/“es”';
+			}
+		}
+		else{
+			echo'2. abizena: Lehenengo hizkiak letra larria izan behar du.';
+		}
+	}
+	else{
+		echo'1. abizena: Lehenengo hizkiak letra larria izan behar du.';
+	}
+}	
+else{
+	echo'Izena: Lehenengo hizkiak letra larria izan behar du.';
+}
+
+/*if ($_POST['Espezialitatea']!='Besterik'){
 	$sql="INSERT INTO erabiltzaile (Izena, Abizena1, Abizena2, Eposta, Pasahitza, Telefonoa, Espezialitatea, Interesa) VALUES ('$_POST[Izena]' , '$_POST[Abizena1]' , '$_POST[Abizena2]' , '$_POST[Eposta]' , '$_POST[Pasahitza]' , '$_POST[Telefonoa]' , '$_POST[Espezialitatea]' , '$_POST[Interesa]')";
 }
 else{
 	$sql="INSERT INTO erabiltzaile (Izena, Abizena1, Abizena2, Eposta, Pasahitza, Telefonoa, Espezialitatea, Interesa) VALUES ('$_POST[Izena]' , '$_POST[Abizena1]' , '$_POST[Abizena2]' , '$_POST[Eposta]' , '$_POST[Pasahitza]' , '$_POST[Telefonoa]' , '$_POST[BesteEspezialitatea]' , '$_POST[Interesa]')";
 }
-
+*/
 $ema=mysqli_query($link,$sql);
 
 if (!$ema){
